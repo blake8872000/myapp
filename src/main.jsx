cb9a5e5d9f4496e6970e57709175e2a8e746c5af
Blake Chen <blake8872000@gmail.com>
2015-03-10 16:25:46 +0800

    first build

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ var React=require("react");
 var kse=require("ksana-search");
 var maincomponent = React.createClass({
   getInitialState:function() {
-    return {result:[],tofind:"資生"};
+    return {result:[],tofind:"天才"};
   },
   search:function() {
     kse.search("sample",this.state.tofind,{range:{start:0}},function(err,data){
