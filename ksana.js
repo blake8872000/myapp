80403ecdfd49b8cfc23b7dd384aa30ca53c7a7f5
Blake Chen <blake8872000@gmail.com>
2015-03-10 15:17:16 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/blake8872000/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  387,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T06:51:56.617Z",
+  "2015-03-10T06:51:56.615Z",
+  "2015-03-10T06:51:56.727Z",
+  "2015-03-10T07:02:13.131Z",
+  "2015-03-10T06:51:56.658Z",
+  "2015-03-10T06:51:56.661Z",
+  "2015-03-10T06:51:56.612Z",
+  "2015-03-10T06:51:56.585Z",
+  "2015-03-10T06:57:42.614Z"
+ ],
+ "date": "2015-03-10T07:02:14.102Z"
+})
